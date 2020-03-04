32156173048bf7daa16270fb37b18bb596c9dc0d
Apik Zorian <apikzorian>
2020-03-03 19:55:52 -0500

    Files for saving video frames

---
+++ docker_images/apik_position_work/video_frame_saver.py
@@ -0,0 +1,13 @@
+import cv2
+import time
+vidcap = cv2.VideoCapture('chute04_cam8_1s.m4v')
+success,image = vidcap.read()
+count = 10
+offset = 0
+while success:
+    success,image = vidcap.read()
+    if offset % 3 == 0:
+        cv2.imwrite("./frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
+        print('Read a new frame: ', success)
+        count += 1
+    offset += 1
